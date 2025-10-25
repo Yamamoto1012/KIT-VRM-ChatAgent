0e00e4dc0b07e7152981b1482ce248bf00b2de8c
Yamamoto1012 <koki.yamamoto03@gmail.com>
2025-10-25 11:39:53 +0900

    feat: 表情管理系のフックをエクスポート

---
+++ frontend/src/features/VRM/VRMExpression/hooks/index.ts
@@ -0,0 +1,9 @@
+/**
+ * VRM表情管理フックのエクスポート
+ */
+
+export { useExpressionManager } from "./useExpressionManager";
+export type {
+	ExpressionManagerActions,
+	ExpressionManagerState,
+} from "./useExpressionManager";
